680704a8274714900a76f2c5b3520bdca07bc1a8
yaxi_liu <1065508630@qq.com>
2022-10-29 13:12:13 +0800

    测试线程

--- GithubTest/Form1.cs
+++ GithubTest/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,7 +20,14 @@ namespace GithubTest
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Thread thread = new Thread(() => 
+            {
+                while (true)
+                {
+                    Console.WriteLine("测试分析");
+                    Thread.Sleep(1000);
+                }
+            });
         }
     }
 }
